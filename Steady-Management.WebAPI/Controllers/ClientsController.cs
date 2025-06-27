81fe93596dcfe71ae26952a9e47d68c41606636c
VeroMari <veronicaagueroaguilar@gmail.com>
2025-06-27 12:36:31 -0600

    Controller Client actualizado

--- Steady-Management.WebAPI/Controllers/ClientsController.cs
+++ Steady-Management.WebAPI/Controllers/ClientsController.cs
@@ -1,35 +1,85 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
-using SteadyManagement.Business;
+﻿using Microsoft.AspNetCore.Mvc;
+using Steady_Management.Business;
 using SteadyManagement.Domain;
+using System;
+using System.Collections.Generic;
 
-namespace SteadyManagement.WebAPI.Controllers
+namespace Steady_Management.WebApi.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
-    public class ClientsController : ControllerBase
+    [Route("api/[controller]")]
+    public class ClientController : ControllerBase
     {
-        private readonly ClientBusiness _bl;
-        public ClientsController(IConfiguration cfg)
+        private readonly ClientBusiness clientBusiness;
+
+        public ClientController(IConfiguration configuration)
         {
-            _bl = new ClientBusiness(cfg.GetConnectionString("SteadyDB"));
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            clientBusiness = new ClientBusiness(connectionString);
         }
 
-        [HttpGet] public IActionResult Get()               => Ok(_bl.GetAll());
-        [HttpGet("{id:int}")]
-        public IActionResult Get(int id)                   => _bl.GetById(id) is { } c ? Ok(c) : NotFound();
+        // GET: api/client
+        [HttpGet]
+        public ActionResult<List<Client>> GetAll()
+        {
+            var clients = clientBusiness.GetAll();
+            return Ok(clients);
+        }
+
+        // GET: api/client/5
+        [HttpGet("{id}")]
+        public ActionResult<Client> GetById(int id)
+        {
+            var client = clientBusiness.GetById(id);
+            if (client == null)
+                return NotFound();
+
+            return Ok(client);
+        }
+
+        // POST: api/client
         [HttpPost]
-        public IActionResult Post([FromBody] Client c)
+        public ActionResult<Client> Create([FromBody] Client client)
         {
-            _bl.AddClient(c);
-            return CreatedAtAction(nameof(Get), new { id = c.ClientId }, c);
+            try
+            {
+                clientBusiness.Create(client);
+                return CreatedAtAction(nameof(GetById), new { id = client.ClientId }, client);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
-        [HttpPut("{id:int}")]
-        public IActionResult Put(int id, [FromBody] Client c)
+
+        // PUT: api/client/5
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Client client)
         {
-            if (id != c.ClientId) return BadRequest("ID mismatch");
-            return _bl.Update(c) ? NoContent() : NotFound();
+            if (id != client.ClientId)
+                return BadRequest("ID del cliente no coincide con el objeto enviado.");
+
+            try
+            {
+                var result = clientBusiness.Update(client);
+                if (!result) return NotFound();
+                return NoContent(); // 204
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
-        [HttpDelete("{id:int}")] public IActionResult Delete(int id) => _bl.Delete(id) ? NoContent() : NotFound();
+
+        // DELETE: api/client/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var deleted = clientBusiness.Delete(id);
+            if (!deleted)
+                return NotFound();
+
+            return NoContent();
+        }
     }
 }
