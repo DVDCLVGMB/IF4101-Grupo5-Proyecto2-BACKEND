8f18b9e103db49443604d163a5cc42ab1dda3e74
VeroMari <veronicaagueroaguilar@gmail.com>
2025-07-02 00:52:40 -0600

    Cambios en departamentoData

--- Steady-Management.DataAccess/DepartmentData.cs
+++ Steady-Management.DataAccess/DepartmentData.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
 using Steady_Management.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace Steady_Management.DataAccess
 {
@@ -17,117 +18,82 @@ namespace Steady_Management.DataAccess
         // -------------------  CREATE  -------------------
         public void Create(Department d)
         {
-            const string sql = @"
-                INSERT INTO Department (dept_name)
-                VALUES (@Name);
-                SELECT SCOPE_IDENTITY();";
-
             using var cn = new SqlConnection(_conn);
+            using var cmd = new SqlCommand("dbo.usp_CreateDepartment", cn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd.Parameters.AddWithValue("@Name", d.DeptName);
+            var pId = new SqlParameter("@NewId", SqlDbType.Int) { Direction = ParameterDirection.Output };
+            cmd.Parameters.Add(pId);
+
             cn.Open();
+            cmd.ExecuteNonQuery();
 
-            using var tx = cn.BeginTransaction();
-            try
-            {
-                using var cmd = new SqlCommand(sql, cn, tx);
-                cmd.Parameters.AddWithValue("@Name", d.DeptName);
-
-                // Recupera el ID generado
-                d.DeptId = Convert.ToInt32(cmd.ExecuteScalar());
-
-                tx.Commit();
-            }
-            catch (SqlException ex)
-            {
-                tx.Rollback();
-                // Aquí podrías registrar el error en un logger antes de relanzar
-                throw new Exception("Error al crear el departamento.", ex);
-            }
+            d.DeptId = (int)pId.Value;
         }
 
-        // -------------------  READ ALL  -----------------
         public List<Department> GetAll()
         {
-            const string sql = "SELECT * FROM Department";
-            var list = new List<Department>();
-
             using var cn = new SqlConnection(_conn);
-            using var cmd = new SqlCommand(sql, cn);
+            using var cmd = new SqlCommand("dbo.usp_GetAllDepartments", cn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
             cn.Open();
             using var rd = cmd.ExecuteReader();
-
+            var list = new List<Department>();
             while (rd.Read())
                 list.Add(Map(rd));
-
             return list;
         }
 
-        // -------------------  READ BY ID  ---------------
         public Department? GetById(int id)
         {
-            const string sql = "SELECT * FROM Department WHERE dept_id = @Id";
-
             using var cn = new SqlConnection(_conn);
-            using var cmd = new SqlCommand(sql, cn);
+            using var cmd = new SqlCommand("dbo.usp_GetDepartmentById", cn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
             cmd.Parameters.AddWithValue("@Id", id);
             cn.Open();
-
             using var rd = cmd.ExecuteReader();
             return rd.Read() ? Map(rd) : null;
         }
 
-        // -------------------  UPDATE  -------------------
         public bool Update(Department d)
         {
-            const string sql = @"
-                UPDATE Department
-                   SET dept_name = @Name
-                 WHERE dept_id   = @Id";
-
             using var cn = new SqlConnection(_conn);
+            using var cmd = new SqlCommand("dbo.usp_UpdateDepartment", cn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd.Parameters.AddWithValue("@Id", d.DeptId);
+            cmd.Parameters.AddWithValue("@Name", d.DeptName);
+            var pRows = new SqlParameter("@RowsAffected", SqlDbType.Int) { Direction = ParameterDirection.Output };
+            cmd.Parameters.Add(pRows);
+
             cn.Open();
-
-            using var tx = cn.BeginTransaction();
-            try
-            {
-                using var cmd = new SqlCommand(sql, cn, tx);
-                cmd.Parameters.AddWithValue("@Name", d.DeptName);
-                cmd.Parameters.AddWithValue("@Id", d.DeptId);
-
-                var rows = cmd.ExecuteNonQuery();
-                tx.Commit();
-                return rows > 0;
-            }
-            catch (SqlException ex)
-            {
-                tx.Rollback();
-                throw new Exception("Error al actualizar el departamento.", ex);
-            }
+            cmd.ExecuteNonQuery();
+            return (int)pRows.Value > 0;
         }
 
-        // -------------------  DELETE  -------------------
         public bool Delete(int id)
         {
-            const string sql = "DELETE FROM Department WHERE dept_id = @Id";
-
             using var cn = new SqlConnection(_conn);
+            using var cmd = new SqlCommand("dbo.usp_DeleteDepartment", cn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd.Parameters.AddWithValue("@Id", id);
+            var pRows = new SqlParameter("@RowsAffected", SqlDbType.Int) { Direction = ParameterDirection.Output };
+            cmd.Parameters.Add(pRows);
+
             cn.Open();
-
-            using var tx = cn.BeginTransaction();
-            try
-            {
-                using var cmd = new SqlCommand(sql, cn, tx);
-                cmd.Parameters.AddWithValue("@Id", id);
-
-                var rows = cmd.ExecuteNonQuery();
-                tx.Commit();
-                return rows > 0;
-            }
-            catch (SqlException ex)
-            {
-                tx.Rollback();
-                throw new Exception("Error al eliminar el departamento.", ex);
-            }
+            cmd.ExecuteNonQuery();
+            return (int)pRows.Value > 0;
         }
+
 
         // -------------------  MAPPER  -------------------
         private static Department Map(SqlDataReader r) =>
